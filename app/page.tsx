951febda31c0ef3266a72b15d9eee5eadd28f8d9
pacodinucci <franciscoldinucci@gmail.com>
2023-11-10 08:46:18 -0300

    landing bg avif

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default async function Home() {
                     <input type="email" placeholder="Email" className="flex-1 px-4 py-2 border-2 bg-transparent border-slate-500 rounded focus:outline-none focus:ring focus:border-slate-700" />
                   </div>
                   <textarea name="" id="" cols={30} rows={11} placeholder="Escriba su mensaje..." className="px-4 py-2 border-2 bg-transparent border-slate-500 rounded focus:outline-none focus:ring focus:border-slate-700"></textarea>
-                  <input type="submit" className="w-full sm:w-auto px-6 py-2 border border-transparent text-white bg-slate-500 rounded hover:bg-slate-700 cursor-pointer" />
+                  <input type="submit" className="w-full sm:w-auto px-6 py-2 border border-transparent text-white bg-slate-500 rounded hover:bg-slate-700 cursor-pointer" value="Enviar" />
                 </form>
             </div>
           </div>
