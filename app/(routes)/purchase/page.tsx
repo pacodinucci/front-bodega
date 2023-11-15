e4161c2215ea366ee70814ddde32e6c7ad3d83e7
pacodinucci <franciscoldinucci@gmail.com>
2023-11-15 10:16:34 -0300

    purchase route

---
+++ app/(routes)/purchase/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import toast from "react-hot-toast";
+
+import Container from "@/components/ui/Container";
+import useCart from "@/hooks/use-cart";
+import Image from "next/image";
+
+const PurchasePage = () => {
+
+    const searchParams = useSearchParams();
+    const removeAll = useCart((state) => state.removeAll);
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    useEffect(() => {
+        if(searchParams.get("success")) {
+            toast.success("Pago completado.")
+            removeAll();
+        }
+    }, [])
+
+    return (
+        <div className="bg-white">
+            <Container>
+                <div className="px-4 py-16 sm:px-6 lg:px-8">
+                    <Image
+                        src="/logogaviotas.svg"
+                        alt="Logo gaviotas"
+                        width={150}
+                        height={0}
+                    />
+                    <h1 className="text-3xl font-bold text-black">Gracias por tu compra!</h1>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default PurchasePage;
